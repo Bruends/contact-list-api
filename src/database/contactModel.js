7de206fd948c444e8793a6c1151aa52c6ef7efbf
Bruno Mendes <brunomendes962@gmail.com>
2018-01-04 14:13:44 -0200

    expanding contact model

--- src/database/contactModel.js
+++ src/database/contactModel.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const contactSchema = new mongoose.Schema({
-  name: {type: String, required: true},
+  name: { type: String, required: true },
   number: String,
-  email: String,
+  email: {
+    type: String,
+    validate: [isEmail, 'Please use a valid email']
+  },
+  twitter: String,
+  facebook: String,
   photo: String
 });
 
